003bbf3431c035329bac3bf144f4fd0bc9f050de
Ad1kk <108807255+Ad1kk@users.noreply.github.com>
2022-10-08 23:10:25 +0600

    Add files via upload

---
+++ DZ-5 4 month/dz5/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './App.css';
+import FormPage from "./FromPage"
+
+function App() {
+  return (
+      <div className="App">
+        <FormPage/>
+      </div>
+  );
+}
+
+export default App;
